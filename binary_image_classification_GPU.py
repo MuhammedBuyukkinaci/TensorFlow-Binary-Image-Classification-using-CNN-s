a047bae274ecec35f3e161d2b532ba0f01481cf2
MuhammedBuyukkinaci <mubuyuk51@gmail.com>
2018-07-08 19:05:13 +0300

    Change due tab-space

--- binary_image_classification_GPU.py
+++ binary_image_classification_GPU.py
@@ -40,10 +40,10 @@ test_data = np.load('test_data_bi.npy')
 
 #In order to implement ALEXNET, we are resizing them to (227,227,3)
 for i in range(len(train_data)):
-	train_data[i][0] = cv2.resize(train_data[i][0],(IMG_SIZE_ALEXNET,IMG_SIZE_ALEXNET))
+    train_data[i][0] = cv2.resize(train_data[i][0],(IMG_SIZE_ALEXNET,IMG_SIZE_ALEXNET))
 
 for i in range(len(test_data)):
-	test_data[i][0] = cv2.resize(test_data[i][0],(IMG_SIZE_ALEXNET,IMG_SIZE_ALEXNET))
+    test_data[i][0] = cv2.resize(test_data[i][0],(IMG_SIZE_ALEXNET,IMG_SIZE_ALEXNET))
 
 train = train_data[:5600]
 cv = train_data[5600:]
